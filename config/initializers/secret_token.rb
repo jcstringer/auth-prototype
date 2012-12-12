5bfa2744313451ac7c58fe44dadf2a43bf17427e
Chris Stringer <chris.stringer@g5searchmarketing.com>
2012-12-12 15:29:48 -0800

    Default Rails install minus the assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '0d49f86ce4b898792fde2e699c44391a716e2d3e0070504df3a42956f0f9e70c2e47405fa381050c6323d5615a88f2073dc9ee2d1440cbff02957bfa18021577'
